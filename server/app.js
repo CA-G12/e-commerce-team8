042cedcca7fc85cbb1370d05015bcb2051a22fae
unknown <saritadh74@gmail.com>
2022-09-26 01:14:47 +0300

    eslint fixed and installed some packages for the client side

--- server/app.js
+++ server/app.js
@@ -3,7 +3,10 @@ const { join } = require('path');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 
+const {NODE_ENV } = require()
+
 const router = require('./routes');
+
 const app = express();
 app.use(cookieParser());
 app.disable('x-powered-by');
@@ -11,11 +14,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
-app.use(express.static(join(__dirname, '..', 'client', 'build')));
+if (NODE_ENV === 'develoment'){
+  app.use(express.static(join(__dirname, '..', 'client', 'build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(join(__dirname, '..', 'client', 'build'));
-});
+  app.get('*', (req, res) => {
+    res.sendFile(join(__dirname, '..', 'client', 'build'));
+  });
+}
 
 app.use(router)
 
