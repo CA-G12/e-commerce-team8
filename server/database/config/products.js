0bd645e7bf548cf98c09d8312c514e82a793b730
naghamabuwarda <nasomabuwarda@gmail.com>
2022-09-26 16:36:22 +0300

    adding product relates #5

---
+++ server/database/config/products.js
@@ -0,0 +1,289 @@
+/* eslint-disable no-unused-vars */
+module.exports = [
+  {
+    title: "Champion Kids Hooded",
+    price: 17,
+    imgURL: "https://m.media-amazon.com/images/I/71MGTC0lZGL._AC_UY550_.jpg",
+    category: "kids",
+    description:
+      "Classic Champion Script Chest Logo - The signature Champion logo represents over 100 years of outfitting athletes and competitors.",
+  },
+  {
+    title: "Champion Boys Tee Shirt Kids Tops",
+    price: 12,
+    imgURL: "https://m.media-amazon.com/images/I/81OuSE18WBL._AC_UX569_.jpg",
+    category: "kids",
+    description:
+      "GUARANTEED AUTHENTIC CHAMPION APPAREL: No imitations, this is authentic Champion kids athletic apparel.",
+  },
+  {
+    title: "PATPAT Kids ",
+    price: 18,
+    imgURL: "https://m.media-amazon.com/images/I/712hmt54p3L._AC_UX569_.jpg",
+    category: "kids",
+    description:
+      "Soft,comfortable and breathable fabric perfect for kids.Soft fabric to help protect child's delicate skin, very comfortable and durable to wear.",
+  },
+  {
+    title: "Aza Boutique Girl's Cute Bunny Sweatshirt",
+    price: 23,
+    imgURL: "https://m.media-amazon.com/images/I/51Eh-hTYAXL._AC_UX569_.jpg",
+    category: "kids",
+    description: "Material: Cotton Blend, Original and high quality",
+  },
+  {
+    title: "GAMISOTE Kids Girl's Fuzzy Hoodies",
+    price: 25,
+    imgURL: "https://m.media-amazon.com/images/I/71-9xGSuKeL._AC_UX466_.jpg",
+    category: "kids",
+    description:
+      " MATERIAL: Polyester, Soft, lightweight, comfy fluffy fleece, perfect for autumn/winter chilly days.",
+  },
+  {
+    title: "Sweater",
+    price: 25,
+    imgURL: "https://m.media-amazon.com/images/I/71MwKOv8wuL._AC_UX569_.jpg",
+    category: "kids",
+    description:
+      "QUALITY FABRIC: Kids pullovers made with super soft and skin friendly material, mid-weight comfy knitwear, perfect for spring fall winter wear",
+  },
+  {
+    title: "5-14 Years Boys Button Down Coat",
+    price: 23,
+    imgURL: "https://m.media-amazon.com/images/I/6190rcVStcL._AC_UX569_.jpg",
+    category: "kids",
+    description:
+      "Features: Boys casual coat shirts, button down, front patch pockets, long sleeve, turn-down collar, loose fit jacket, drop shoulder, single breasted The basic outerwear jackets is made of comfortable and skin-friendly fabric which is suitable for spring, fall and winter",
+  },
+  {
+    title: "Carter's Boys' Fleece",
+    price: 20,
+    imgURL: "https://m.media-amazon.com/images/I/81r4W61zkQL._AC_UX569_.jpg",
+    category: "kids",
+    description: "MONSTER SCREEN PRINT ON FRONT",
+  },
+  {
+    title: "Kids Boy's Girl's Jacket",
+    price: 38,
+    imgURL: "https://m.media-amazon.com/images/I/71yrkHbV9aL._AC_UX569_.jpg",
+    category: "kids",
+    description:
+      "Adorable boys Jackets or hoodie, a breathable, soft, and comfortable Dark Yellow and Blue long-sleeves casual comfy warm zip up sweatshirt to keep them comfortable and warm",
+  },
+  {
+    title:
+      "Gihuo Unisex Boy's Girls Winter Warm Sherpa Lined Crewneck Pullover Sweatshirt",
+    price: 28,
+    imgURL: "https://m.media-amazon.com/images/I/816Xw16fvBL._AC_UX679_.jpg",
+    category: "kids",
+    description: "Pullover closure, Solid color, round neck",
+  },
+  //   ------------------------------------------------------------
+  {
+    title: "YAHUIUL Parachute Pants for Women",
+    price: 31,
+    imgURL: "https://m.media-amazon.com/images/I/51PWAqcym0L._AC_UY550_.jpg",
+    category: "pants",
+    description:
+      "Parachute pants for women are made of premium fabric, wearable, skin-friendly, soft, comfortable for all-day wear",
+  },
+  {
+    title: "Amazon Essentials Men's Pant",
+    price: 15,
+    imgURL: "https://m.media-amazon.com/images/I/71TOtMq54ML._AC_UX569_.jpg",
+    category: "pants",
+    description:
+      "This slim-fit chino pant features wrinkle-free fabric, a flat-front design, and button-through back welt pockets for a tailored look and all-day comfort",
+  },
+  {
+    title: "Arizona Tie Pant Women's, Safari, Large",
+    price: 50,
+    imgURL: "https://m.media-amazon.com/images/I/71u0SkXi4cS._AC_UX569_.jpg",
+    category: "pants",
+    description: "Tie top, Skinny leg",
+  },
+  {
+    title: "Monrow Women's Sporty Sweatpants",
+    price: 135,
+    imgURL: "https://m.media-amazon.com/images/I/91QDOXMj5oL._AC_UY550_.jpg",
+    category: "pants",
+    description:
+      "YOUR MOST FLATTERING SWEATPANT: With the same flattering straight leg cut as our classic vintage sweats, these sweatpants make things even comfier with a drawstring closure so that you can find your perfect fit.",
+  },
+  {
+    title: "Women's Supersoft Sweater Knit Cuffed Joggers",
+    price: 156,
+    imgURL: "https://m.media-amazon.com/images/I/61ipowTQ9bL._AC_UX679_.jpg",
+    category: "pants",
+    description:
+      "FOR MORE THAN LOUNGING: Cozy enough for the couch and still cute enough for errands. Gym, coffee, and dog walk approved.",
+  },
+  {
+    title: "Women's 90's Classic Joggers, Flattering Straight Leg Cut",
+    price: 168,
+    imgURL: "https://m.media-amazon.com/images/I/51wTSayWD6L._AC_UX569_.jpg",
+    category: "pants",
+    description:
+      "FOR MORE THAN LOUNGING: Cozy enough for the couch and still cute enough for errands. Great for everything from work-from-home days to casual coffee runs, workouts, and chill lunches out.",
+  },
+  {
+    title: "CRZ YOGA Womens Sweatpants",
+    price: 34,
+    imgURL: "https://m.media-amazon.com/images/I/71sL84JXI9L._AC_UY550_.jpg",
+    category: "pants",
+    description:
+      "Inseam: 27 inches, intended to sit at ankle. High Waist. Semi-slim Fit",
+  },
+  {
+    title: "Match Men's Wild Cargo Pants",
+    price: 40,
+    imgURL: "https://m.media-amazon.com/images/I/51lrvBS8lRL._AC_UX569_.jpg",
+    category: "pants",
+    description:
+      "Sits at waist / loose fit / straight legCold machine / hand wash separately Notice: Belt not included, please buy separately",
+  },
+  {
+    title: "Good Man Brand Men's",
+    price: 148,
+    imgURL: "https://m.media-amazon.com/images/I/61cMuT90+0S._AC_UX679_.jpg",
+    category: "pants",
+    description:
+      "MODERN AND VERSATILE - You can only spend so much time behind a desk, and a day spent in motion is much more satisfying. Spend it looking good both in the office and on the move in your Pro Stretch Twill Star Chino casual pants for men.",
+  },
+  {
+    title: "Men's Golf Pants",
+    price: 47,
+    imgURL: "https://m.media-amazon.com/images/I/71P0zVxGEcL._AC_UY550_.jpg",
+    category: "pants",
+    description:
+      "62% cotton, comfortable breathable and moisture; 35% polyester, prevent color fading and being out of shape to make sure the bright vivid color and good shape; 3% spandex, offer stretch room for golf sports",
+  },
+  //   ------------------------------------------------------
+  {
+    title: "Janet Band Jackson T-Shirts",
+    price: 23,
+    imgURL: "https://m.media-amazon.com/images/I/61nCOKK2i5L._AC_UX569_.jpg",
+    category: "shirt",
+    description:
+      "100% Cotton.The Selection Of Environmentally Friendly Fabrics Allows You To Dress More Comfortably.",
+  },
+  {
+    title: "The Drop Women's Sydney Short-sleeve Cropped Crew Neck T-shirt",
+    price: 19,
+    imgURL: "https://m.media-amazon.com/images/I/91vmWaCA58L._AC_UY550_.jpg",
+    category: "shirt",
+    description: "Staples by The Drop",
+  },
+  {
+    title: "BELONGSCI Women's Dress Sweet",
+    price: 14,
+    imgURL: "https://m.media-amazon.com/images/I/51ZjAjaaW1L._AC_UX522_.jpg",
+    category: "dress",
+    description:
+      "Perfect outfit for summer and autumn daily wear, school, casual, beach, vacation, date, party, club, etc.",
+  },
+  {
+    title: "Amoretu Women Summer Tunic Dress",
+    price: 26,
+    imgURL: "https://m.media-amazon.com/images/I/61UQcTVpA1L._AC_UY550_.jpg",
+    category: "dress",
+    description:
+      "Deep v neckline with lantern long sleeve/short sleeve/sleeveless, super sexy, fashionable and elegant.",
+  },
+  {
+    title: "ECOWISH Women",
+    price: 30,
+    imgURL: "https://m.media-amazon.com/images/I/71aVecbFY1L._AC_UY550_.jpg",
+    category: "dress",
+    description:
+      "Material - This long sleeve fall dress is made of VISCOSE. You will feel comfortable to skin, soft and breathable.",
+  },
+  {
+    title: "MEIZHINV Women Summer Casual Dress",
+    price: 32,
+    imgURL: "https://m.media-amazon.com/images/I/619KhyOJ8hL._AC_UX466_.jpg",
+    category: "dress",
+    description:
+      "Material: 100% Polyester. Breathable And Comfortable, Skin-Friendly And Comfy To Wear, Bringing You A Pleasant Dressing Experience.",
+  },
+  {
+    title: "FARYSAYS Women's Dresses",
+    price: 37,
+    imgURL: "https://m.media-amazon.com/images/I/61w8s7vS-0L._AC_UY550_.jpg",
+    category: "dress",
+    description:
+      "Feature: Ruffle, Waist Tie, Round Neck, Long Sleeve, Elastic Waist, Back Hole, A-Line, Swing Flared, Above Knee Length, Casual mini dresses fits for any body shape.",
+  },
+  {
+    title: "BELONGSCI Women's Dress Sweet",
+    price: 32,
+    imgURL: "https://m.media-amazon.com/images/I/51vroLbml5L._AC_UX522_.jpg",
+    category: "dress",
+    description:
+      "Dressy cute dress, V-neck, long sleeve, mini short dress, nice material and comfortable,non-see through,casual perfect fitting, best choice for casual/wedding/cocktail/party/work/ business. Suitable for spring and summer wear.",
+  },
+  //   -------------------------------------------------------------------
+  {
+    title: "Nike Women's Basketball Shoe",
+    price: 120,
+    imgURL: "https://m.media-amazon.com/images/I/613HmnmaO6L._AC_UX575_.jpg",
+    category: "shose",
+    description: "Women's Basketball Shoes",
+  },
+  {
+    title: "Nike Women's Race Running Shoe",
+    price: 141,
+    imgURL: "https://m.media-amazon.com/images/I/61aDXpWqAEL._AC_UY575_.jpg",
+    category: "shose",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    title: "Nike Zoom Air Fire Womens Running Trainers Cw3876 Sneakers Shoes",
+    price: 105,
+    imgURL: "https://m.media-amazon.com/images/I/71+02iaGsKL._AC_UY695_.jpg",
+    category: "shose",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    title: "Nike Women's Quest 3 Running Shoe",
+    price: 98,
+    imgURL: "https://m.media-amazon.com/images/I/61wa7SuCzxL._AC_UX575_.jpg",
+    category: "shose",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    title: "Agirlvct Pearl Hair Claw",
+    price: 10,
+    imgURL: "https://m.media-amazon.com/images/I/71UVPFOnT4L._SX466_.jpg",
+    category: "accessories",
+    description:
+      "Big Hair Clips Barrettes Nonslip Birthday Business Gift Hair Accessories for Women Girls Daughter Girlfriend(4 Pack)",
+  },
+  {
+    title: "Unicra Leaf Wedding Hair Vine Flower",
+    price: 0,
+    imgURL: "https://m.media-amazon.com/images/I/61o-IUIh50L._AC_SX679_.jpg",
+    category: "accessories",
+    description:
+      "Rose gold leaves hair accessories in unique design and excellent workmanship, made of flowers, crystals, rhinestones, and alloy.",
+  },
+  {
+    title: "6 Pieces Pearl ",
+    price: 8,
+    imgURL: "https://m.media-amazon.com/images/I/71GiTLq7q4S._SX466_.jpg",
+    category: "accessories",
+    description:
+      "What you receive: 6 pieces of bride hair pins in total, these simple and classical hair pins are good for wedding, prom and party",
+  },
+  {
+    title: "Jakawin Bride Wedding Pearl Hair ",
+    price: 13,
+    imgURL: "https://m.media-amazon.com/images/I/61RVJHFW7FL._SX466_.jpg",
+    category: "accessories",
+    description:
+      "Wedding hair pins are made of alloy,pearls, beads and crystal rhinestone.",
+  },
+];
