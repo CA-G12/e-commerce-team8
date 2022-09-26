cb4c2b934f3b19852930fa5283de33535e0c6e39
unknown <saritadh74@gmail.com>
2022-09-26 15:36:29 +0300

    modified the cart query relates #20

--- server/database/queries/products/getCartProductsQuery.js
+++ server/database/queries/products/getCartProductsQuery.js
@@ -2,7 +2,7 @@ const connection = require("../../config/connection");
 
 const getCartProductsQuery = ({ userId })=> connection
     .query(`
-        SELECT products.*, users.* FROM users 
+        SELECT products.*, users.*, cart.product_id FROM users 
         JOIN cart ON cart.user_id = users.id 
         JOIN products ON cart.product_id = products.id 
         WHERE users.id = ($1);
