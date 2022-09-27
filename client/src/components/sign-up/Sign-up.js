c98116d1bf41e28003d7db642b9009a9661f9a87
unknown <saritadh74@gmail.com>
2022-09-27 11:18:05 +0300

    created the sign up in client side relates #31

--- client/src/components/sign-up/Sign-up.js
+++ client/src/components/sign-up/Sign-up.js
@@ -2,9 +2,72 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios'
 import swal from 'sweetalert';
+import './Sign-up.css'
 
 export default function Signup() {
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: '',
+    repeatedPassword: ''
+  });
+
+  const [message, setMessage] =  useState('')
+
+  const handleChange = (e) =>{
+    setUser({
+        ...user,
+        [e.target.name] : e.target.value,
+    })
+  }
+
+  const handleSubmit = (e) =>{
+    const {name, email, password, repeatedPassword} = user;
+    if(!name || !email || !password ||!repeatedPassword) {
+        swal("Warning!", "Please fill in all fields!", "warning");
+    }
+    else {
+        axios.post('/signup', user)
+        .then((res) => {
+            swal("hello", res.data.message , "success")
+            .then(() => {window.location.href = '/'})
+        })
+        .catch((err) => setMessage(err.response.data.message))
+    }
+  }
+  
   return (
-    <div>sign up</div>
+    <div className='signup'>
+        <h2>Sign Up</h2>
+        <div className='signup-form'>
+            <input type='text'
+             value={user.name}
+             name= 'name'
+             placeholder='username'
+             onChange={handleChange}
+            />
+            <input type='email'
+             value={user.email}
+             name = 'email'
+             placeholder='email'
+             onChange={handleChange}
+            />
+            <input type='password'
+              value={user.password}
+              name='password'
+              placeholder='password'
+              onChange={handleChange}
+            />
+            <input type='password'
+              value={user.repeatedPassword}
+              name='repeatedPassword'
+              placeholder='confirm password'
+              onChange={handleChange}
+            />
+
+            <p>{message}</p>
+            <button type='button' onClick={handleSubmit}> Sign Up </button>
+        </div>
+    </div>
   )
 }
