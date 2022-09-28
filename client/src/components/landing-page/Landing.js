1180e37276c674241bbb463e768ae9522a26dd2a
naghamabuwarda <nasomabuwarda@gmail.com>
2022-09-28 14:13:38 +0300

    adding the pagination relates #57

--- client/src/components/landing-page/Landing.js
+++ client/src/components/landing-page/Landing.js
@@ -5,45 +5,63 @@ import axios from "axios";
 import Navbar from "../navbar/Navbar";
 import Categories from "../categories/Categories";
 import CardsContainer from "../cardsContainer/CardsContainer";
-import Card from "../card/Card";
+// import Card from "../card/Card";
+import Pagination from "./Pagination";
 
 export default function Landing() {
   const [data, setData] = useState(null);
+  const [newcategory, setCategory] = useState("All");
   const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
-  const [productsPerPage, setProductsPerPage] = useState(11);
-  const [user, setUser] = useState(undefined);
+  const [productsPerPage, setProductsPerPage] = useState(10);
 
   const categoryHandleChane = (category) => {
+    setCategory(category);
+    if (category !== "All") setCurrentPage(1);
+
     const result = products.filter((ele) =>
       category === "All"
         ? true
         : ele.category.toLowerCase() === category.toLowerCase()
     );
+
     const indexOfLastProduct = currentPage * productsPerPage;
-    const indexOfFirstProduct = currentPage - productsPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - 10;
     const currentProduct = result.slice(
-      (indexOfLastProduct, indexOfFirstProduct)
+      indexOfFirstProduct,
+      indexOfLastProduct
     );
     setData(currentProduct);
   };
 
+  const current = (currentNum) => {
+    setCurrentPage(currentNum);
+  };
+
   useEffect(() => {
     axios.get("/products").then((res) => {
-      setData(res.data.products);
+      setData(res.data.products.slice(0, 10));
       setProducts(res.data.products);
       setUser(res.data.user);
     });
   }, []);
 
-  if (!data) return <div> Loading........</div>;
+  useEffect(() => {
+    categoryHandleChane(newcategory);
+  }, [currentPage]);
 
+  if (!data) return <div> Loading........</div>;
   return (
     <>
-      <Navbar user={user}/>
+      <Navbar user={user} />
       <Categories arr={data} categoryHandleChange={categoryHandleChane} />
-      <CardsContainer arr={data} user={user} />
+      <CardsContainer arr={data} />
+      <Pagination
+        productsPerPage={productsPerPage}
+        totalProducts={50}
+        current={current}
+      />
     </>
   );
 }
